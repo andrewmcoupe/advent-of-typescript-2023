06bd883d26cd63d41a6e6ee72f5313562150a014
Andy Coupe <andrewmcoupe@gmail.com>
2023-12-13 10:29:57 +0000

    add explanation comments

--- src/day-13.ts
+++ src/day-13.ts
@@ -1,14 +1,17 @@
+/*
+ * Increments a given number by one
+ * */
 type IncrementByOne<
   TNumber extends number,
-  Acc extends number[] = [],
-> = TNumber extends Acc["length"]
-  ? [...Acc, number]["length"]
-  : IncrementByOne<TNumber, [...Acc, number]>;
+  $Acc extends number[] = [],
+> = TNumber extends $Acc["length"]
+  ? [...$Acc, number]["length"] // If the number is equal to the length of the accumulator, return the length of the accumulator + 1
+  : IncrementByOne<TNumber, [...$Acc, number]>; // Otherwise, recursively call IncrementByOne with the number and the accumulator, adding a number to the accumulator each time
 
 type DayCounter<
   Start extends number,
   End extends number,
-  Acc extends any[] = [],
-> = Acc["length"] extends End
-  ? Acc[number]
-  : DayCounter<Start, End, [...Acc, IncrementByOne<Acc["length"]>]>;
+  $Acc extends any[] = [],
+> = $Acc["length"] extends End
+  ? $Acc[number]
+  : DayCounter<Start, End, [...$Acc, IncrementByOne<$Acc["length"]>]>;
